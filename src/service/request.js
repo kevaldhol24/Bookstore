0467ee1a993bbf9cf58f13aa7506051f3d399acf
Keval Dhol <79499164+kevaldhol24@users.noreply.github.com>
2023-02-27 11:31:59 +0530

    Update API base URL

--- src/service/request.js
+++ src/service/request.js
@@ -3,8 +3,8 @@ import { toast } from "react-toastify";
 
 const request = axios.create({
   //  baseURL: "https://web1.anasource.com/BookStore/api/BookStore/", // url = base url + request url
-  baseURL: "http://localhost:5000/",
-  // baseURL: "https://ebooksell-api.vercel.app/",
+//   baseURL: "http://localhost:5000/",
+  baseURL: "https://ebooksell-api.vercel.app/",
   // baseURL: "https://helperland1.azurewebsites.net/",
   // baseURL: "https://helperland1.azurewebsites.net/",
   //  baseURL: "http://192.168.1.20/",
